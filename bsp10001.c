efa9560bf0808bec27ea2d1e301fae068dcaf740
Martin <martin_gratt@hotmail.com>
2019-09-25 12:50:50 +0200

    bsp 10001 fertig

--- bsp10001.c
+++ bsp10001.c
@@ -3,5 +3,11 @@
 #include <stdio.h>
 
 main(){
+    
     printf("\nDies ist ein erstes C-Programm. \n");
+    printf("\nWie Sie sehen, kann 'printf' ");
+    printf("nicht nur Texte drucken. \n");
+    printf("sondern auch rechnen. \n\n");
+    printf("13 * 7 = %i \n", 13 * 7);
+
 }
